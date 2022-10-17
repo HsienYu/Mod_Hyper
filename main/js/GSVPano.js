d54cf610d0497c4ee5c10977834fb1f280a70286
chenghsienyu <chenghsienyu@aim.com>
2022-10-17 23:48:03 +0800

    update

--- main/js/GSVPano.js
+++ main/js/GSVPano.js
@@ -81,6 +81,7 @@ GSVPANO.PanoLoader = function (parameters) {
 		for (y = 0; y < h; y++) {
 			for (x = 0; x < w; x++) {
 				url = 'https://maps.google.com/cbk?output=tile&panoid=' + panoId + '&zoom=' + _zoom + '&x=' + x + '&y=' + y + '&' + Date.now();
+				console.log(url);
 				(function (x, y) {
 					var img = new Image();
 					img.addEventListener('load', function () {
